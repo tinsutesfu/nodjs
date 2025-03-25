ffcdfef6fafb9d993e022c508a8f6b9d51f29878
tinsutesfu <153826309+tinsutesfu@users.noreply.github.com>
2025-03-25 04:50:25 -0700

    Update allowedoption.js

--- api/config/allowedoption.js
+++ api/config/allowedoption.js
@@ -3,7 +3,7 @@ const allowedoption = [
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:5173',
-    
+    'https://nodjs-api.onrender.com',
     'https://5899-188-226-162-9.ngrok-free.app'
 ];
 
