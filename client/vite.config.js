3e125b55732fab2acb13c1a1e53c73ec7d763ddb
tinsutesfu <153826309+tinsutesfu@users.noreply.github.com>
2025-03-25 23:15:18 +0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: '.dust', // Ensure the output directory is set to .dust
-  },
-});
+})
