03dc728835e990dc732f877337714fa40fa3049a
tinsutesfu <yetesfayes@gmail.com>
2025-03-27 14:27:12 -0700

    some

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,9 +3,4 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Output to client/dist
-    sourcemap: true, // Optional for debugging
-  },
-  base: '/', // Serve assets from root (matches Render.com)
 });
