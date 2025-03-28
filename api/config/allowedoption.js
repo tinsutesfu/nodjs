e69cc7af3d1892aa61513a424ad546e40c3ae964
tinsutesfu <yetesfayes@gmail.com>
2025-03-28 09:06:16 -0700

    fjjjjjjj

--- api/config/allowedoption.js
+++ api/config/allowedoption.js
@@ -1,6 +1,7 @@
 const allowedoption = [
      'https://nodjs-49td.onrender.com',
-     'https://nodjs-1-3k0d.onrender.com'
+     'https://nodjs-1-3k0d.onrender.com',
+     'http://localhost:5173'
 ];
 
 export default allowedoption;
